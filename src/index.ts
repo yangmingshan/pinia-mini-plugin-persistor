41c76624d3d00540d8dcbf749386c9abaad7315a
Yang Mingshan <y.mingshan3@gmail.com>
2025-03-08 00:47:21 +0800

    feat: persistor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-export const a = 'a'
+import type { Store, StateTree, PiniaPluginContext } from '@vue-mini/pinia'
+import './types'
+
+function hydrate(store: Store) {
+  try {
+    const state: string = wx.getStorageSync(store.$id)
+    if (state) {
+      store.$patch(JSON.parse(state))
+    }
+    // eslint-disable-next-line no-empty
+  } catch {}
+}
+
+function persist(id: string, state: StateTree) {
+  try {
+    wx.setStorageSync(id, JSON.stringify(state))
+    // eslint-disable-next-line no-empty
+  } catch {}
+}
+
+export default function persistor(context: PiniaPluginContext) {
+  const { store, options } = context
+
+  if (!options.persist) return
+
+  store.$hydrate = () => {
+    hydrate(store)
+  }
+
+  store.$persist = () => {
+    persist(store.$id, store.$state)
+  }
+
+  hydrate(store)
+
+  store.$subscribe(
+    (mutation, state) => {
+      persist(mutation.storeId, state)
+    },
+    { detached: true },
+  )
+}
